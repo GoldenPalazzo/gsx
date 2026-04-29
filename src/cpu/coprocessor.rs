21a595e694cdc639fb47d3599fa9b812c7fe6650
Francesco Palazzo <francesco@palazzo.me>
2026-04-29 15:30:02 +0200

    Stubbed COP2 imm25

--- src/cpu/coprocessor.rs
+++ src/cpu/coprocessor.rs
@@ -65,6 +65,21 @@ impl Coprocessor for Gte {
         }
     }
     fn exec(&mut self, cmd: u32) {
-        todo!("{:08X}", cmd)
+        // todo!("{:08X}", cmd)
+        let real_cmd = cmd & 0x3f;
+        let saturate = (cmd & (1 << 10)) != 0;
+        let mvma_trans = (cmd >> 13) & 3;
+        let mvma_mul_vec = (cmd >> 15) & 3;
+        let mvma_mul_matr = (cmd >> 17) & 3;
+        let shift_fac = (cmd & (1 << 19)) != 0;
+        println!(
+            "Silent stub COP2 {:025b} {} {} {} {} {} {}",
+            cmd, real_cmd, saturate, mvma_trans, mvma_mul_vec, mvma_mul_matr, shift_fac
+        );
+        // silent stub
+        // panic!(
+        //     "{:025b} {} {} {} {} {} {} ",
+        //     cmd, real_cmd, saturate, mvma_trans, mvma_mul_vec, mvma_mul_matr, shift_fac
+        // );
     }
 }
