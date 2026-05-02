df3a5277260ec2e5ad5dc9844ec9d8170c14c815
Francesco Palazzo <francesco@palazzo.me>
2026-05-02 23:38:41 +0200

    Allow to load BIOS without bin/exe

--- src/main.rs
+++ src/main.rs
@@ -11,26 +11,30 @@ fn wait_for_enter() {
 }
 
 fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: {} <file.bin/exe>", args[0]);
-        return;
-    }
-    let fpath = &args[1];
-    let data = fs::read(fpath).expect("Can't read file");
     let bios = std::fs::read("SCPH1001.BIN").expect("BIOS SCPH1001.BIN not found!");
 
     let mut cpu;
     let mut mem = crate::memory::MemoryBus::with_bios(&bios);
-    if &data[0..8] == b"PS-X EXE" {
-        let (pc, r28, r29_30) = mem.load_psexe(&data);
-        cpu = crate::cpu::Cpu::new(pc, r28, r29_30, r29_30);
+    if args.len() > 1 {
+        let fpath = &args[1];
+        let data = fs::read(fpath).expect("Can't read file");
+        if &data[0..8] == b"PS-X EXE" {
+            let (pc, r28, r29_30) = mem.load_psexe(&data);
+            cpu = crate::cpu::Cpu::new(pc, r28, r29_30, r29_30);
+        } else {
+            cpu = crate::cpu::Cpu::default();
+        }
     } else {
         cpu = crate::cpu::Cpu::default();
     }
 
     loop {
-        wait_for_enter();
+        // wait_for_enter();
         cpu.step(&mut mem);
     }
 }
